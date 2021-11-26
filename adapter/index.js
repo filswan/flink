c027dc066cc73c570a6c4d2ae47c25caee7c9f82
Charles Cao <ccao@nbai.io>
2021-11-26 16:03:28 -0500

    official document

--- adapter/index.js
+++ adapter/index.js
@@ -12,25 +12,33 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  deal: ['deal'],
-  // quote: ['quote', 'data', 'dealCid']
+  base: ['base', 'from', 'coin'],
+  quote: ['quote', 'to', 'market'],
+  endpoint: false
 }
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const deal = validator.validated.data.deal 
-  console.log(validator.validated.data);
-  const url = `https://calibration-api.filscout.com/api/v1/storagedeal/${deal}`
- 
+  const endpoint = validator.validated.data.endpoint || 'price'
+  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
+  const fsym = validator.validated.data.base.toUpperCase()
+  const tsyms = validator.validated.data.quote.toUpperCase()
+
+  const params = {
+    fsym,
+    tsyms
+  }
+
   // This is where you would add method and headers
   // you can add method like GET or POST and add it to the config
   // The default is GET requests
   // method = 'get' 
   // headers = 'headers.....'
   const config = {
-    url
+    url,
+    params
   }
 
   // The Requester allows API calls be retry in case of timeout
@@ -40,8 +48,7 @@ const createRequest = (input, callback) => {
       // It's common practice to store the desired value at the top-level
       // result key. This allows different adapters to be compatible with
       // one another.
-      const result = {dealCid: response.data.data.dealCid, price: response.data.data.storagePricePerEpoch}
-      response.data.result = result
+      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
       callback(response.status, Requester.success(jobRunID, response))
     })
     .catch(error => {
