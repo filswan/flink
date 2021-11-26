bf4b3a2d99ef438b740264da5fde90e0c569862d
Charles Cao <ccao@nbai.io>
2021-11-26 16:06:54 -0500

    add calibration filecoin test

--- adapter/index.js
+++ adapter/index.js
@@ -12,48 +12,41 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
-  endpoint: false
+  deal: ['deal'],
+  // quote: ['quote', 'data', 'dealCid']
 }
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
-
-  const params = {
-    fsym,
-    tsyms
-  }
+  const deal = validator.validated.data.deal
+  console.log(validator.validated.data);
+  const url = `https://calibration-api.filscout.com/api/v1/storagedeal/${deal}`
 
   // This is where you would add method and headers
   // you can add method like GET or POST and add it to the config
   // The default is GET requests
-  // method = 'get' 
+  // method = 'get'
   // headers = 'headers.....'
   const config = {
-    url,
-    params
+    url
   }
 
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
   Requester.request(config, customError)
-    .then(response => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
-    })
-    .catch(error => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
+      .then(response => {
+        // It's common practice to store the desired value at the top-level
+        // result key. This allows different adapters to be compatible with
+        // one another.
+        const result = {dealCid: response.data.data.dealCid, price: response.data.data.storagePricePerEpoch}
+        response.data.result = result
+        callback(response.status, Requester.success(jobRunID, response))
+      })
+      .catch(error => {
+        callback(500, Requester.errored(jobRunID, error))
+      })
 }
 
 // This is a wrapper to allow the function to work with
