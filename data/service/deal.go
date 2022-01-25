059c5807869f9f6d1925d087430c8d79120218a7
Danny Nguyen <dnguyen@nbai.io>
2022-01-25 11:13:08 -0500

    Add network id to all APIs(Update)

--- data/service/deal.go
+++ data/service/deal.go
@@ -2,18 +2,20 @@ package service
 
 import (
 	"flink-data/models"
+
+	"github.com/filswan/go-swan-lib/logs"
 )
 
 func GetDealById(dealId int64, networkId int) (*models.ChainLinkDealBase, error) {
 	dealInternal, err := models.GetDealByIdAndNetwork(dealId, networkId)
 	if err != nil {
-		//logs.GetLogger().Error(err)
+		logs.GetLogger().Error(err)
 		return nil, err
 	}
 
 	network, err := models.GetNetworkById(dealInternal.NetworkId)
 	if err != nil {
-		//logs.GetLogger().Error(err)
+		logs.GetLogger().Error(err)
 		return nil, err
 	}
 	dealInternal.NetworkName = network.Name
@@ -32,13 +34,13 @@ func GetLatestDealByNetwork(networkId int64) (*models.ChainLinkDealBase, error) 
 
 	dealInternal, err := models.GetDealByIdAndNetwork(maxDealId, int(networkId))
 	if err != nil {
-		//logs.GetLogger().Error(err)
+		logs.GetLogger().Error(err)
 		return nil, err
 	}
 
 	network, err := models.GetNetworkById(dealInternal.NetworkId)
 	if err != nil {
-		//logs.GetLogger().Error(err)
+		logs.GetLogger().Error(err)
 		return nil, err
 	}
 	dealInternal.NetworkName = network.Name
