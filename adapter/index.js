79133b2a074b0678571c1ebddc236e45e1e60448
yiming liu <leviliushmh@gmail.com>
2022-06-02 14:35:26 -0400

    only one rqeust is enough

--- adapter/index.js
+++ adapter/index.js
@@ -37,67 +37,78 @@ const createRequest = (method, input, callback) => {
   const config = {
     url: `${url}`,
     method: 'POST',
-    data: {network_name: `${network}` , deal_id: `${deal}` }
+    data: { network_name: `${network}`, deal_id: `${deal}` }
   }
 
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
-  if (method === "post") {
+  //   if (method === "post") {
+  //   Requester.request(config, customError)
+  //       .then(response => {
+  //         // It's common practice to store the desired value at the top-level
+  //         // result key. This allows different adapters to be compatible with
+  //         // one another.
+  //         // const result = {dealCid: response.data.data.dealCid, price: response.data.data.storagePricePerEpoch}
+  //         // response.data.result = result
+  //         callback(response.status, Requester.success(jobRunID, response))
+  //       })
+  //       .catch(error => {
+  //         callback(500, Requester.errored(jobRunID, error))
+  //       })
+  //     } 
+  //     else {
+  //       Requester.request(config, customError)
+  //       .then(response => {
+  //         // It's common practice to store the desired value at the top-level
+  //         // result key. This allows different adapters to be compatible with
+  //         // one another.
+  //         callback(response.status, Requester.success(jobRunID, response))
+  //       })
+  //       .catch(error => {
+  //         callback(500, Requester.errored(jobRunID, error))
+  //       })
+  //     }    
+  // }
+
   Requester.request(config, customError)
-      .then(response => {
-        // It's common practice to store the desired value at the top-level
-        // result key. This allows different adapters to be compatible with
-        // one another.
-        const result = {dealCid: response.data.data.dealCid, price: response.data.data.storagePricePerEpoch}
-        response.data.result = result
-        callback(response.status, Requester.success(jobRunID, response))
-      })
-      .catch(error => {
-        callback(500, Requester.errored(jobRunID, error))
-      })
-    } 
-    else {
-      Requester.request(config, customError)
-      .then(response => {
-        // It's common practice to store the desired value at the top-level
-        // result key. This allows different adapters to be compatible with
-        // one another.
-        callback(response.status, Requester.success(jobRunID, response))
-      })
-      .catch(error => {
-        callback(500, Requester.errored(jobRunID, error))
-      })
-    }    
-}
-
-// This is a wrapper to allow the function to work with
-// GCP Functions
-exports.gcpservice = (req, res) => {
-  createRequest(req.body, (statusCode, data) => {
-    res.status(statusCode).send(data)
-  })
-}
-
-// This is a wrapper to allow the function to work with
-// AWS Lambda
-exports.handler = (event, context, callback) => {
-  createRequest(event, (statusCode, data) => {
-    callback(null, data)
-  })
-}
-
-// This is a wrapper to allow the function to work with
-// newer AWS Lambda implementations
-exports.handlerv2 = (event, context, callback) => {
-  createRequest(JSON.parse(event.body), (statusCode, data) => {
-    callback(null, {
-      statusCode: statusCode,
-      body: JSON.stringify(data),
-      isBase64Encoded: false
+    .then(response => {
+      // It's common practice to store the desired value at the top-level
+      // result key. This allows different adapters to be compatible with
+      // one another.
+      callback(response.status, Requester.success(jobRunID, response))
     })
-  })
-}
+    .catch(error => {
+      callback(500, Requester.errored(jobRunID, error))
+    })
 
-// This allows the function to be exported for testing
-// or for running in express
-module.exports.createRequest = createRequest
+  // This is a wrapper to allow the function to work with
+  // GCP Functions
+  exports.gcpservice = (req, res) => {
+    createRequest(req.body, (statusCode, data) => {
+      res.status(statusCode).send(data)
+    })
+  }
+
+  // This is a wrapper to allow the function to work with
+  // AWS Lambda
+  exports.handler = (event, context, callback) => {
+    createRequest(event, (statusCode, data) => {
+      callback(null, data)
+    })
+  }
+
+  // This is a wrapper to allow the function to work with
+  // newer AWS Lambda implementations
+  exports.handlerv2 = (event, context, callback) => {
+    createRequest(JSON.parse(event.body), (statusCode, data) => {
+      callback(null, {
+        statusCode: statusCode,
+        body: JSON.stringify(data),
+        isBase64Encoded: false
+      })
+    })
+  }
+
+  // This allows the function to be exported for testing
+  // or for running in express
+  module.exports.createRequest = createRequest
