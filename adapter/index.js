a3554b18146b8c99b50ddd30edcbbdd8761400e1
yiming liu <leviliushmh@gmail.com>
2022-06-02 16:35:16 -0400

    restore to old version

--- adapter/index.js
+++ adapter/index.js
@@ -42,7 +42,22 @@ const createRequest = (method, input, callback) => {
 
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
+  if (method === "post") {
   Requester.request(config, customError)
+      .then(response => {
+        // It's common practice to store the desired value at the top-level
+        // result key. This allows different adapters to be compatible with
+        // one another.
+        // const result = {dealCid: response.data.data.dealCid, price: response.data.data.storagePricePerEpoch}
+        // response.data.result = result
+        callback(response.status, Requester.success(jobRunID, response))
+      })
+      .catch(error => {
+        callback(500, Requester.errored(jobRunID, error))
+      })
+    } 
+    else {
+      Requester.request(config, customError)
       .then(response => {
         // It's common practice to store the desired value at the top-level
         // result key. This allows different adapters to be compatible with
@@ -52,6 +67,7 @@ const createRequest = (method, input, callback) => {
       .catch(error => {
         callback(500, Requester.errored(jobRunID, error))
       })
+    }    
 }
 
 // This is a wrapper to allow the function to work with
